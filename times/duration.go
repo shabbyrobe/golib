615e922047352854f7ea3c105ea292a98320662b
shabbyrobe <code@shabbyrobe.org>
2017-12-28 22:44:10 +1100

    Time helpers

--- times/duration.go
+++ times/duration.go
@@ -11,23 +11,6 @@ const (
 	week = 24 * 7 * time.Hour
 )
 
-// func FirstDayOfISOWeek(t time.Time) (tm time.Time, year, week int) {
-//     s := time.Date(t.Year(), t.Month(), t.Day(), 0, 0, 0, 0, t.Location())
-//     y, w := s.ISOWeek()
-//
-//     for {
-//         n := s.Add(-24 * time.Hour)
-//         ny, nw := n.ISOWeek()
-//         if ny != y || nw != w {
-//             break
-//         }
-//         s, y, w = n, ny, nw
-//     }
-//
-//     s = time.Date(s.Year(), s.Month(), s.Day(), 0, 0, 0, 0, t.Location())
-//     return s, y, w
-// }
-
 func FirstDayOfISOWeek(t time.Time) (tm time.Time, year, week int) {
 	s := time.Date(t.Year(), t.Month(), t.Day(), 0, 0, 0, 0, t.Location())
 	if s.Weekday() != time.Monday {
@@ -54,6 +37,13 @@ func FirstDayOfWeek(t time.Time) time.Time {
 }
 
 func TruncateWeeks(t time.Time, n int) time.Time {
+	p := PeriodWeeks(t, n)
+	return PeriodWeeksTime(p, n, t.Location())
+}
+
+// PeriodWeeks returns a monotonically increasing/decreasing integer that
+// represents a period of n weeks since the Unix epoch.
+func PeriodWeeks(t time.Time, n int) int {
 	ts := FirstDayOfWeek(t)
 	diff := ts.Sub(firstDayOfEpochWeek)
 	weeks := int(diff / week)
@@ -64,9 +54,18 @@ func TruncateWeeks(t time.Time, n int) time.Time {
 	} else {
 		gap = weeks - n - (weeks % n)
 	}
+	return gap / n
+}
 
-	out := firstDayOfEpochWeek.Add(time.Duration(gap) * week)
-	out = time.Date(out.Year(), out.Month(), out.Day(), 0, 0, 0, 0, t.Location())
+// PeriodWeeksTime returns a time for the integer identifying the period of
+// n weeks since the Unix epoch.
+func PeriodWeeksTime(p int, n int, loc *time.Location) time.Time {
+	if loc == nil {
+		loc = time.UTC
+	}
+	p *= n
+	out := firstDayOfEpochWeek.Add(time.Duration(p) * week)
+	out = time.Date(out.Year(), out.Month(), out.Day(), 0, 0, 0, 0, loc)
 	return out
 }
 
@@ -89,6 +88,39 @@ func TruncateMonths(t time.Time, n int) time.Time {
 
 		return time.Date(oy, time.Month(om), 1, 0, 0, 0, 0, t.Location())
 	}
+}
+
+func PeriodMonths(t time.Time, n int) int {
+	inMnth := (t.Year() * 12) + (int(t.Month()) - 1)
+	if inMnth >= 0 {
+		return (inMnth - (inMnth % n)) / n
+	} else {
+		out := inMnth
+		gap := inMnth % n
+		if gap != 0 {
+			out -= n + gap
+		}
+		return out / n
+	}
+}
+
+func PeriodMonthsTime(p int, n int, loc *time.Location) time.Time {
+	if loc == nil {
+		loc = time.UTC
+	}
+	p *= n
+	ms := p
+
+	var oy, om int
+	if p >= 0 {
+		oy = (ms / 12)
+		om = (ms % 12) + 1
+	} else {
+		oy = (ms / 12) - 1
+		om = 12 - (-ms % 12) + 1
+	}
+
+	return time.Date(oy, time.Month(om), 1, 0, 0, 0, 0, loc)
 }
 
 func AddMonths(t time.Time, n int) time.Time {
