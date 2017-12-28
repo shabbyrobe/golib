1137f6d21200c32538e554729584154641ff5a62
shabbyrobe <code@shabbyrobe.org>
2017-12-28 12:20:04 +1100

    Add time helpers

---
+++ times/duration.go
@@ -0,0 +1,108 @@
+package times
+
+import "time"
+
+var (
+	firstDayOfEpochWeek = FirstDayOfWeek(time.Unix(0, 0))
+	epoch               = time.Unix(0, 0)
+)
+
+const (
+	week = 24 * 7 * time.Hour
+)
+
+// func FirstDayOfISOWeek(t time.Time) (tm time.Time, year, week int) {
+//     s := time.Date(t.Year(), t.Month(), t.Day(), 0, 0, 0, 0, t.Location())
+//     y, w := s.ISOWeek()
+//
+//     for {
+//         n := s.Add(-24 * time.Hour)
+//         ny, nw := n.ISOWeek()
+//         if ny != y || nw != w {
+//             break
+//         }
+//         s, y, w = n, ny, nw
+//     }
+//
+//     s = time.Date(s.Year(), s.Month(), s.Day(), 0, 0, 0, 0, t.Location())
+//     return s, y, w
+// }
+
+func FirstDayOfISOWeek(t time.Time) (tm time.Time, year, week int) {
+	s := time.Date(t.Year(), t.Month(), t.Day(), 0, 0, 0, 0, t.Location())
+	if s.Weekday() != time.Monday {
+		diff := int(s.Weekday() - time.Monday)
+		if diff < 0 {
+			diff = 6
+		}
+		s = s.Add(-(24 * time.Duration(diff) * time.Hour))
+	}
+	y, w := s.ISOWeek()
+	return s, y, w
+}
+
+func FirstDayOfWeek(t time.Time) time.Time {
+	s := time.Date(t.Year(), t.Month(), t.Day(), 0, 0, 0, 0, t.Location())
+	if s.Weekday() != time.Monday {
+		diff := int(s.Weekday() - time.Monday)
+		if diff < 0 {
+			diff = 6
+		}
+		s = s.Add(-(24 * time.Duration(diff) * time.Hour))
+	}
+	return s
+}
+
+func TruncateWeeks(t time.Time, n int) time.Time {
+	ts := FirstDayOfWeek(t)
+	diff := ts.Sub(firstDayOfEpochWeek)
+	weeks := int(diff / week)
+
+	var gap int
+	if diff >= 0 {
+		gap = weeks - (weeks % n)
+	} else {
+		gap = weeks - n - (weeks % n)
+	}
+
+	out := firstDayOfEpochWeek.Add(time.Duration(gap) * week)
+	out = time.Date(out.Year(), out.Month(), out.Day(), 0, 0, 0, 0, t.Location())
+	return out
+}
+
+func TruncateMonths(t time.Time, n int) time.Time {
+	if n == 1 {
+		return time.Date(t.Year(), t.Month(), 1, 0, 0, 0, 0, t.Location())
+	} else {
+		inMnth := (t.Year() * 12) + (int(t.Month()) - 1)
+
+		var oy, om int
+		if inMnth >= 0 {
+			ms := inMnth - (inMnth % n)
+			oy = (ms / 12)
+			om = (ms % 12) + 1
+		} else {
+			ms := inMnth + (inMnth % n)
+			oy = (ms / 12) - 1
+			om = 12 - (-ms % 12) + 1
+		}
+
+		return time.Date(oy, time.Month(om), 1, 0, 0, 0, 0, t.Location())
+	}
+}
+
+func AddMonths(t time.Time, n int) time.Time {
+	inMnth := (t.Year() * 12) + (int(t.Month()) - 1)
+	trg := inMnth + n
+
+	var oy, om int
+	oy = (trg / 12)
+	om = (trg % 12) + 1
+
+	return time.Date(oy, time.Month(om), t.Day(), t.Hour(), t.Minute(), t.Second(), t.Nanosecond(), t.Location())
+}
+
+func AddYears(t time.Time, n int) time.Time {
+	return time.Date(t.Year()+n, t.Month(), t.Day(),
+		t.Hour(), t.Minute(), t.Second(), t.Nanosecond(), t.Location())
+}
---
+++ times/duration_test.go
@@ -0,0 +1,212 @@
+package times
+
+import (
+	"math/rand"
+	"testing"
+	"time"
+
+	"github.com/shabbyrobe/golib/assert"
+)
+
+func TestFirstDayOfWeek(t *testing.T) {
+	tt := assert.WrapTB(t)
+
+	iter := 20000
+
+	start := time.Date(2000, 01, 01, 0, 0, 0, 0, time.UTC)
+
+	for i := 0; i < iter; i++ {
+		f := start.Add(time.Duration(i) * 24 * time.Hour)
+		n := FirstDayOfWeek(f)
+		tt.MustEqual(time.Monday, n.Weekday(), f.String())
+	}
+
+	for i := 0; i < iter; i++ {
+		f := start.Add(time.Duration(i) * 24 * time.Hour)
+		f = f.Add(time.Duration(rand.Intn(60)) * time.Minute)
+		f = f.Add(time.Duration(rand.Intn(24)) * time.Hour)
+		n := FirstDayOfWeek(f)
+		tt.MustEqual(time.Monday, n.Weekday(), f.String())
+	}
+}
+
+func TestTruncateMonths(t *testing.T) {
+	tt := assert.WrapTB(t)
+
+	tm := TruncateMonths(time.Date(1970, 1, 14, 0, 0, 0, 0, time.UTC), 2)
+	exp := time.Date(1970, 1, 1, 0, 0, 0, 0, time.UTC)
+	tt.MustEqual(exp, tm)
+
+	tm = TruncateMonths(time.Date(1970, 2, 3, 0, 0, 0, 0, time.UTC), 2)
+	exp = time.Date(1970, 1, 1, 0, 0, 0, 0, time.UTC)
+	tt.MustEqual(exp, tm)
+
+	tm = TruncateMonths(time.Date(1970, 3, 1, 0, 0, 0, 0, time.UTC), 2)
+	exp = time.Date(1970, 3, 1, 0, 0, 0, 0, time.UTC)
+	tt.MustEqual(exp, tm)
+
+	tm = TruncateMonths(time.Date(-1, 12, 31, 23, 59, 0, 0, time.UTC), 2)
+	exp = time.Date(-1, 11, 1, 0, 0, 0, 0, time.UTC)
+	tt.MustEqual(exp, tm)
+
+	tm = TruncateMonths(time.Date(-1, 11, 01, 23, 59, 0, 0, time.UTC), 2)
+	exp = time.Date(-1, 11, 1, 0, 0, 0, 0, time.UTC)
+	tt.MustEqual(exp, tm)
+
+	tm = TruncateMonths(time.Date(-1, 11, 01, 0, 0, 0, 0, time.UTC), 2)
+	exp = time.Date(-1, 11, 1, 0, 0, 0, 0, time.UTC)
+	tt.MustEqual(exp, tm)
+
+	tm = TruncateMonths(time.Date(-1, 10, 31, 0, 0, 0, 0, time.UTC), 2)
+	exp = time.Date(-1, 9, 1, 0, 0, 0, 0, time.UTC)
+	tt.MustEqual(exp, tm)
+
+	// 3 months:
+
+	tm = TruncateMonths(time.Date(1970, 1, 14, 0, 0, 0, 0, time.UTC), 3)
+	exp = time.Date(1970, 1, 1, 0, 0, 0, 0, time.UTC)
+	tt.MustEqual(exp, tm)
+
+	tm = TruncateMonths(time.Date(1970, 2, 3, 0, 0, 0, 0, time.UTC), 3)
+	exp = time.Date(1970, 1, 1, 0, 0, 0, 0, time.UTC)
+	tt.MustEqual(exp, tm)
+
+	tm = TruncateMonths(time.Date(1970, 3, 3, 0, 0, 0, 0, time.UTC), 3)
+	exp = time.Date(1970, 1, 1, 0, 0, 0, 0, time.UTC)
+	tt.MustEqual(exp, tm)
+
+	tm = TruncateMonths(time.Date(1970, 4, 1, 0, 0, 0, 0, time.UTC), 3)
+	exp = time.Date(1970, 4, 1, 0, 0, 0, 0, time.UTC)
+	tt.MustEqual(exp, tm)
+
+	tm = TruncateMonths(time.Date(1969, 12, 31, 23, 59, 0, 0, time.UTC), 3)
+	exp = time.Date(1969, 10, 1, 0, 0, 0, 0, time.UTC)
+	tt.MustEqual(exp, tm)
+
+	tm = TruncateMonths(time.Date(1969, 11, 01, 23, 59, 0, 0, time.UTC), 3)
+	exp = time.Date(1969, 10, 1, 0, 0, 0, 0, time.UTC)
+	tt.MustEqual(exp, tm)
+
+	tm = TruncateMonths(time.Date(1969, 11, 01, 0, 0, 0, 0, time.UTC), 3)
+	exp = time.Date(1969, 10, 1, 0, 0, 0, 0, time.UTC)
+	tt.MustEqual(exp, tm)
+
+	tm = TruncateMonths(time.Date(1969, 10, 31, 0, 0, 0, 0, time.UTC), 3)
+	exp = time.Date(1969, 10, 1, 0, 0, 0, 0, time.UTC)
+	tt.MustEqual(exp, tm)
+
+	tm = TruncateMonths(time.Date(1969, 9, 30, 0, 0, 0, 0, time.UTC), 3)
+	exp = time.Date(1969, 7, 1, 0, 0, 0, 0, time.UTC)
+	tt.MustEqual(exp, tm)
+}
+
+func TestTruncateWeeks(t *testing.T) {
+	tt := assert.WrapTB(t)
+
+	tm := TruncateWeeks(time.Date(1970, 1, 14, 0, 0, 0, 0, time.UTC), 2)
+	exp := time.Date(1970, 1, 12, 0, 0, 0, 0, time.UTC)
+	tt.MustEqual(exp, tm)
+
+	tm = TruncateWeeks(time.Date(1970, 1, 21, 0, 0, 0, 0, time.UTC), 2)
+	exp = time.Date(1970, 1, 12, 0, 0, 0, 0, time.UTC)
+	tt.MustEqual(exp, tm)
+
+	tm = TruncateWeeks(time.Date(1970, 1, 28, 0, 0, 0, 0, time.UTC), 2)
+	exp = time.Date(1970, 1, 26, 0, 0, 0, 0, time.UTC)
+	tt.MustEqual(exp, tm)
+
+	tm = TruncateWeeks(time.Date(2017, 1, 28, 12, 30, 0, 0, time.UTC), 2)
+	exp = time.Date(2017, 1, 23, 0, 0, 0, 0, time.UTC)
+	tt.MustEqual(exp, tm)
+
+	tm = TruncateWeeks(time.Date(1970, 1, 1, 0, 0, 0, 0, time.UTC), 2)
+	exp = time.Date(1969, 12, 29, 0, 0, 0, 0, time.UTC)
+	tt.MustEqual(exp, tm)
+
+	tm = TruncateWeeks(time.Date(1969, 12, 29, 0, 0, 0, 0, time.UTC), 2)
+	exp = time.Date(1969, 12, 29, 0, 0, 0, 0, time.UTC)
+	tt.MustEqual(exp, tm)
+
+	tm = TruncateWeeks(time.Date(1969, 12, 28, 0, 0, 0, 0, time.UTC), 2)
+	exp = time.Date(1969, 12, 15, 0, 0, 0, 0, time.UTC)
+	tt.MustEqual(exp, tm)
+
+	tm = TruncateWeeks(time.Date(1969, 12, 16, 0, 0, 0, 0, time.UTC), 2)
+	exp = time.Date(1969, 12, 15, 0, 0, 0, 0, time.UTC)
+	tt.MustEqual(exp, tm)
+
+	tm = TruncateWeeks(time.Date(1969, 12, 15, 0, 0, 0, 0, time.UTC), 2)
+	exp = time.Date(1969, 12, 15, 0, 0, 0, 0, time.UTC)
+	tt.MustEqual(exp, tm)
+
+	tm = TruncateWeeks(time.Date(1969, 12, 14, 0, 0, 0, 0, time.UTC), 2)
+	exp = time.Date(1969, 12, 1, 0, 0, 0, 0, time.UTC)
+	tt.MustEqual(exp, tm)
+
+	// 3 weeks
+	tm = TruncateWeeks(time.Date(1970, 1, 21, 0, 0, 0, 0, time.UTC), 3)
+	exp = time.Date(1970, 1, 19, 0, 0, 0, 0, time.UTC)
+	tt.MustEqual(exp, tm)
+
+	tm = TruncateWeeks(time.Date(1970, 1, 28, 0, 0, 0, 0, time.UTC), 3)
+	exp = time.Date(1970, 1, 19, 0, 0, 0, 0, time.UTC)
+	tt.MustEqual(exp, tm)
+
+	tm = TruncateWeeks(time.Date(1970, 2, 5, 0, 0, 0, 0, time.UTC), 3)
+	exp = time.Date(1970, 1, 19, 0, 0, 0, 0, time.UTC)
+	tt.MustEqual(exp, tm)
+
+	tm = TruncateWeeks(time.Date(1970, 2, 12, 0, 0, 0, 0, time.UTC), 3)
+	exp = time.Date(1970, 2, 9, 0, 0, 0, 0, time.UTC)
+	tt.MustEqual(exp, tm)
+
+	tm = TruncateWeeks(time.Date(1969, 12, 29, 0, 0, 0, 0, time.UTC), 3)
+	exp = time.Date(1969, 12, 29, 0, 0, 0, 0, time.UTC)
+	tt.MustEqual(exp, tm)
+
+	tm = TruncateWeeks(time.Date(1969, 12, 28, 0, 0, 0, 0, time.UTC), 3)
+	exp = time.Date(1969, 12, 8, 0, 0, 0, 0, time.UTC)
+	tt.MustEqual(exp, tm)
+
+	tm = TruncateWeeks(time.Date(1969, 12, 16, 0, 0, 0, 0, time.UTC), 3)
+	exp = time.Date(1969, 12, 8, 0, 0, 0, 0, time.UTC)
+	tt.MustEqual(exp, tm)
+
+	tm = TruncateWeeks(time.Date(1969, 12, 15, 0, 0, 0, 0, time.UTC), 3)
+	exp = time.Date(1969, 12, 8, 0, 0, 0, 0, time.UTC)
+	tt.MustEqual(exp, tm)
+
+	tm = TruncateWeeks(time.Date(1969, 12, 7, 0, 0, 0, 0, time.UTC), 3)
+	exp = time.Date(1969, 11, 17, 0, 0, 0, 0, time.UTC)
+	tt.MustEqual(exp, tm)
+}
+
+func TestAddMonths(t *testing.T) {
+	tt := assert.WrapTB(t)
+	tt.MustEqual(time.Date(2005, 1, 1, 10, 9, 8, 7, time.UTC),
+		AddMonths(time.Date(2004, 11, 1, 10, 9, 8, 7, time.UTC), 2))
+
+	tt.MustEqual(time.Date(2004, 9, 1, 10, 9, 8, 7, time.UTC),
+		AddMonths(time.Date(2004, 11, 1, 10, 9, 8, 7, time.UTC), -2))
+
+	tt.MustEqual(time.Date(0, 1, 1, 10, 9, 8, 7, time.UTC),
+		AddMonths(time.Date(-1, 11, 1, 10, 9, 8, 7, time.UTC), 2))
+
+	tt.MustEqual(time.Date(-1, 1, 1, 10, 9, 8, 7, time.UTC),
+		AddMonths(time.Date(-2, 11, 1, 10, 9, 8, 7, time.UTC), 2))
+}
+
+func TestAddYears(t *testing.T) {
+	tt := assert.WrapTB(t)
+	tt.MustEqual(time.Date(2006, 11, 1, 10, 9, 8, 7, time.UTC),
+		AddYears(time.Date(2004, 11, 1, 10, 9, 8, 7, time.UTC), 2))
+
+	tt.MustEqual(time.Date(2004, 9, 1, 10, 9, 8, 7, time.UTC),
+		AddYears(time.Date(2006, 9, 1, 10, 9, 8, 7, time.UTC), -2))
+
+	tt.MustEqual(time.Date(0, 1, 1, 10, 9, 8, 7, time.UTC),
+		AddYears(time.Date(-2, 1, 1, 10, 9, 8, 7, time.UTC), 2))
+
+	tt.MustEqual(time.Date(-1, 11, 1, 10, 9, 8, 7, time.UTC),
+		AddYears(time.Date(-3, 11, 1, 10, 9, 8, 7, time.UTC), 2))
+}
