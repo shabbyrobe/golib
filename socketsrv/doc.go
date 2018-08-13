93896f597176aef9876c61c8b85e089a680273fa
shabbyrobe <code@shabbyrobe.org>
2018-08-13 17:29:48 +1000

    socketsrv: package level docks

---
+++ socketsrv/doc.go
@@ -0,0 +1,6 @@
+/*
+Package socketsrv implements a versatile framework for creating socket servers
+with arbitrary, interchangeable, versioned protocols using a request/response
+pattern.
+*/
+package socketsrv
---
+++ socketsrv/packetsrv/doc.go
@@ -0,0 +1,7 @@
+/*
+Package packetsrv is an experiment to see if it's possible to use the machinery
+of socketsrv with a PacketConn-based connection. There's probably a lot of
+details that make this infeasible in practice; this package will be used to
+experiment and find those details. Please don't use it.
+*/
+package packetsrv
