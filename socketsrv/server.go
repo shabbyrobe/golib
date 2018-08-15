f9da4c9c29683253eafb2f0d0a58981376b5e386
shabbyrobe <code@shabbyrobe.org>
2018-08-15 18:31:12 +1000

    socketsrv: server convenience methods

--- socketsrv/server.go
+++ socketsrv/server.go
@@ -5,10 +5,31 @@ import (
 	"fmt"
 	"sync"
 	"sync/atomic"
+	"time"
 
 	service "github.com/shabbyrobe/go-service"
 	"github.com/shabbyrobe/golib/incrementer"
 )
+
+var serverRunner = service.NewRunner()
+
+type (
+	ServerOption       func(srv *Server)
+	OnServerConnect    func(server *Server, id ConnID)
+	OnServerDisconnect func(server *Server, id ConnID, err error)
+)
+
+// ServerConnect is a ServerOption that registers a callback that happens when
+// a client connects to the server.
+func ServerConnect(cb OnServerConnect) ServerOption {
+	return func(srv *Server) { srv.onConnect = cb }
+}
+
+// ServerDisconnect is a ServerOption that registers a callback that happens when
+// a client disconnects from the server.
+func ServerDisconnect(cb OnServerDisconnect) ServerOption {
+	return func(srv *Server) { srv.onDisconnect = cb }
+}
 
 type Server struct {
 	config       ServerConfig
@@ -23,20 +44,6 @@ type Server struct {
 	conns   map[ConnID]*conn
 	connsMu sync.Mutex
 	running uint32
-}
-
-type (
-	ServerOption       func(srv *Server)
-	OnServerConnect    func(server *Server, id ConnID)
-	OnServerDisconnect func(server *Server, id ConnID, err error)
-)
-
-func ServerConnect(cb OnServerConnect) ServerOption {
-	return func(srv *Server) { srv.onConnect = cb }
-}
-
-func ServerDisconnect(cb OnServerDisconnect) ServerOption {
-	return func(srv *Server) { srv.onDisconnect = cb }
 }
 
 func NewServer(config ServerConfig, listener Listener, negotiator Negotiator, handler Handler, opts ...ServerOption) *Server {
@@ -85,6 +92,28 @@ func (srv *Server) onEnd(stage service.Stage, svc *service.Service, err error) {
 	}()
 }
 
+func (srv *Server) MustServe(host string) {
+	if err := srv.Serve(host); err != nil {
+		panic(err)
+	}
+}
+
+// Serve is a shorthand for starting the Server as a service using
+// github.com/shabbyrobe/go-service.
+func (srv *Server) Serve(host string) error {
+	ender := service.NewEndListener(1)
+	svc := service.New(service.Name(host), srv).WithEndListener(ender)
+
+	// Hard-coded timeout shouldn't be an issue here.
+	if err := service.StartTimeout(10*time.Second, serverRunner, svc); err != nil {
+		return err
+	}
+	return <-ender.Ends()
+}
+
+// Run implements the Runnable interface in github.com/shabbyrobe/go-service.
+//
+// See Serve() for an example of how to start a service in a runner.
 func (srv *Server) Run(ctx service.Context) (rerr error) {
 	if !atomic.CompareAndSwapUint32(&srv.running, 0, 1) {
 		return fmt.Errorf("socket: server already running")
@@ -112,30 +141,34 @@ func (srv *Server) Run(ctx service.Context) (rerr error) {
 			raw, err := srv.listener.Accept()
 			if err != nil {
 				ctx.OnError(err)
-
-			} else {
-				go func() {
-					id := ConnID(srv.nextID.Next())
-					conn := newConn(id, ServerSide, srv.config.Conn, raw, srv.negotiator, srv.handler)
-
-					// we must start the service and raise the onConnected event
-					// while the lock is acquired otherwise the "onDisconnect"
-					// callback can be called before the "onConnect" callback.
-					srv.connsMu.Lock()
-					if err := srv.runner.Start(ctx, service.New(service.Name(id), conn)); err != nil {
-						srv.connsMu.Unlock()
-
-						_ = raw.Close()
-						ctx.OnError(err)
-						return
-					}
-					if srv.onConnect != nil {
-						srv.onConnect(srv, id)
-					}
-					srv.conns[id] = conn
-					srv.connsMu.Unlock()
-				}()
+				continue
 			}
+
+			// Even though we are using go-service for backgrounding, this
+			// still needs to be a goroutine. The connection is not considered
+			// "started" until version negotiation is complete, but version
+			// negotiation may hit the network.
+			go func() {
+				id := ConnID(srv.nextID.Next())
+				conn := newConn(id, ServerSide, srv.config.Conn, raw, srv.negotiator, srv.handler)
+
+				// we must start the service and raise the onConnected event
+				// while the lock is acquired otherwise the "onDisconnect"
+				// callback can be called before the "onConnect" callback.
+				srv.connsMu.Lock()
+				if err := srv.runner.Start(ctx, service.New(service.Name(id), conn)); err != nil {
+					srv.connsMu.Unlock()
+
+					_ = raw.Close()
+					ctx.OnError(err)
+					return
+				}
+				if srv.onConnect != nil {
+					srv.onConnect(srv, id)
+				}
+				srv.conns[id] = conn
+				srv.connsMu.Unlock()
+			}()
 		}
 	}()
 
