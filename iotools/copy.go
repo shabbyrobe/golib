92d0a9c5525d2fda83bb528f414fe6bfdd4f7d83
shabbyrobe <code@shabbyrobe.org>
2018-08-26 01:26:05 +1000

    iotools: check expected copy bytes

--- iotools/copy.go
+++ iotools/copy.go
@@ -1,6 +1,7 @@
 package iotools
 
 import (
+	"fmt"
 	"io"
 	"os"
 
@@ -14,12 +15,42 @@ func CopyFile(from, to string) (rerr error) {
 	}
 	defer in.Close()
 
+	st, err := in.Stat()
+	if err != nil {
+		return err
+	}
+
+	expected := st.Size()
+
 	out, err := os.Create(to)
 	if err != nil {
 		return err
 	}
 	defer errtools.DeferClose(&rerr, out)
 
-	_, err = io.Copy(out, in)
-	return err
+	n, err := io.Copy(out, in)
+	if err != nil {
+		return err
+	}
+
+	if n != expected {
+		return fmt.Errorf("iotools: copy expected %d bytes, but only copied %d", expected, n)
+	}
+
+	return nil
+}
+
+// MoveFile copies a file from one location to another, then removes the
+// original if the copy succeeded.
+//
+// It is useful for situations where it is known that os.Rename would produce
+// an "invalid cross-device link" error
+func MoveFile(from, to string) (rerr error) {
+	if err := CopyFile(from, to); err != nil {
+		return err
+	}
+	if err := os.Remove(from); err != nil {
+		return err
+	}
+	return nil
 }
