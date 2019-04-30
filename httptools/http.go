7847e1eb26c2270185fb15f1c1a796b50c2c16b7
shabbyrobe <code@shabbyrobe.org>
2019-05-01 01:32:28 +1000

    httptools: fix CORS handler

--- httptools/http.go
+++ httptools/http.go
@@ -7,12 +7,23 @@ import (
 	"time"
 )
 
+func CORSOpen(handler http.Handler, origin string) CORSHandler {
+	return CORSHandler{
+		Handler:          handler,
+		AllowOrigin:      origin,
+		AllowCredentials: true,
+		AllowMethods:     []string{"*"},
+		AllowHeaders:     []string{"*"},
+	}
+}
+
 type CORSHandler struct {
-	Handler      http.Handler
-	AllowOrigin  string
-	AllowMethods []string
-	AllowHeaders []string
-	MaxAge       time.Duration
+	Handler          http.Handler
+	AllowCredentials bool
+	AllowOrigin      string
+	AllowMethods     []string
+	AllowHeaders     []string
+	MaxAge           time.Duration
 }
 
 func (c CORSHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
@@ -22,15 +33,26 @@ func (c CORSHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		} else {
 			w.Header().Set("Access-Control-Allow-Origin", origin)
 		}
-		if len(c.AllowHeaders) > 0 {
+
+		if c.AllowCredentials {
+			w.Header().Set("Access-Control-Allow-Credentials", "true")
+		}
+
+		if len(c.AllowHeaders) == 1 && c.AllowHeaders[0] == "*" {
+			w.Header().Set("Access-Control-Allow-Headers", r.Header.Get("Access-Control-Request-Headers"))
+
+		} else if len(c.AllowHeaders) > 0 {
 			w.Header().Set("Access-Control-Allow-Headers", strings.Join(c.AllowHeaders, ", "))
 		}
+
 		if len(c.AllowMethods) > 0 {
 			w.Header().Set("Access-Control-Allow-Methods", strings.Join(c.AllowMethods, ", "))
 		}
+
 		if c.MaxAge > 0 {
 			w.Header().Set("Access-Control-Max-Age", fmt.Sprintf("%d", c.MaxAge*time.Second))
 		}
+
 		if r.Method == "OPTIONS" {
 			w.WriteHeader(http.StatusOK)
 			return
