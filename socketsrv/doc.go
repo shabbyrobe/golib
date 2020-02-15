bc8975880646fca8a6b4bffd3947d2813cb37c81
Blake Williams <code@shabbyrobe.org>
2020-02-15 13:37:07 +1100

    Move socketsrv to sockerball
    http://github.com/shabbyrobe/sockerball

--- socketsrv/doc.go
+++
@@ -1,6 +0,0 @@
-/*
-Package socketsrv implements a versatile framework for creating socket servers
-with arbitrary, interchangeable, versioned protocols using a request/response
-pattern.
-*/
-package socketsrv
