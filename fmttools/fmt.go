e5cc2a3e67f4141d79e92ef78fbed276cf7cd265
Blake Williams <code@shabbyrobe.org>
2020-02-15 11:57:33 +1100

    fmttools: Explain FormatInt a little better

--- fmttools/fmt.go
+++ fmttools/fmt.go
@@ -2,9 +2,23 @@ package fmttools
 
 import "fmt"
 
-// FormatInt makes it a bit easier to implement custom formatting
-// that can fall back to formatting as decimal. It assumes a rune of
-// 'd' is passed.
+// FormatInt makes it a bit easier to implement a '%d' format branch for a type
+// that implements fmt.Formatter. It assumes a rune of 'd' is passed.
+//
+// For example:
+//
+//   type Test int
+//
+//   func (t Test) String() { return fmt.Sprintf("YEP %d YEP", t) }
+//
+//   func (t Test) Format(f fmt.State, c rune) {
+//   	if c == 's' || c == 'v' {
+//   		io.WriteString(f, m.String())
+//   	} else {
+//   		fmttools.FormatInt(int64(m), f)
+//   	}
+//   }
+//
 func FormatInt(v int64, f fmt.State) {
 	zero := f.Flag('0')
 	w, wok := f.Width()
@@ -17,9 +31,8 @@ func FormatInt(v int64, f fmt.State) {
 	}
 }
 
-// FormatUint makes it a bit easier to implement custom formatting
-// that can fall back to formatting as decimal. It assumes a rune of
-// 'd' is passed.
+// FormatInt makes it a bit easier to implement a '%d' format branch for a type
+// that implements fmt.Formatter. It assumes a rune of 'd' is passed.
 func FormatUint(v uint64, f fmt.State) {
 	zero := f.Flag('0')
 	w, wok := f.Width()
