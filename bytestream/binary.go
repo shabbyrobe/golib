8b73c048171e5b89034f3a6c8ca65bb47818e498
Blake Williams <code@shabbyrobe.org>
2021-05-18 01:24:44 +1000

    bytestream: A different approach for binary

--- bytestream/binary.go
+++ bytestream/binary.go
@@ -5,39 +5,21 @@ import (
 	"fmt"
 )
 
-type Binary interface {
-	Uint8Within(min, max uint8) (uint8, error)
-	Uint16Within(min, max uint16) (uint16, error)
-	Uint32Within(min, max uint32) (uint32, error)
-	Uint64Within(min, max uint64) (uint64, error)
-	Int8Within(min, max int8) (int8, error)
-	Int16Within(min, max int16) (int16, error)
-	Int32Within(min, max int32) (int32, error)
-	Int64Within(min, max int64) (int64, error)
-	Uint8() (uint8, error)
-	Uint16() (uint16, error)
-	Uint32() (uint32, error)
-	Uint64() (uint64, error)
-	Int8() (int8, error)
-	Int16() (int16, error)
-	Int32() (int32, error)
-	Int64() (int64, error)
-}
+var (
+	leOrder = binary.LittleEndian
+	beOrder = binary.BigEndian
+)
 
-var leOrder = binary.LittleEndian
-
-type LittleEndian struct {
+type Binary struct {
 	ByteStream
 }
 
-var _ Binary = &LittleEndian{}
-
-func NewLittleEndian(stream ByteStream) *LittleEndian {
-	return &LittleEndian{stream}
+func NewBinary(stream ByteStream) *Binary {
+	return &Binary{stream}
 }
 
-func (le *LittleEndian) Uint8Within(min, max uint8) (uint8, error) {
-	v, err := le.ByteStream.ReadByte()
+func (bin *Binary) Uint8Within(min, max uint8) (uint8, error) {
+	v, err := bin.ByteStream.ReadByte()
 	if err != nil {
 		return 0, err
 	}
@@ -47,8 +29,20 @@ func (le *LittleEndian) Uint8Within(min, max uint8) (uint8, error) {
 	return v, nil
 }
 
-func (le *LittleEndian) Uint16Within(min, max uint16) (uint16, error) {
-	b, err := le.ByteStream.TakeExactly(2)
+func (bin *Binary) Uint16Within(order binary.ByteOrder, min, max uint16) (uint16, error) {
+	b, err := bin.ByteStream.TakeExactly(2)
+	if err != nil {
+		return 0, err
+	}
+	v := order.Uint16(b)
+	if v < min || v > max {
+		return v, fmt.Errorf("bytestream: uint16 not in range %d <= %d <= %d", min, v, max)
+	}
+	return v, nil
+}
+
+func (bin *Binary) Uint16LEWithin(min, max uint16) (uint16, error) {
+	b, err := bin.ByteStream.TakeExactly(2)
 	if err != nil {
 		return 0, err
 	}
@@ -59,163 +53,8 @@ func (le *LittleEndian) Uint16Within(min, max uint16) (uint16, error) {
 	return v, nil
 }
 
-func (le *LittleEndian) Uint32Within(min, max uint32) (uint32, error) {
-	b, err := le.ByteStream.TakeExactly(4)
-	if err != nil {
-		return 0, err
-	}
-	v := leOrder.Uint32(b)
-	if v < min || v > max {
-		return v, fmt.Errorf("bytestream: uint32 not in range %d <= %d <= %d", min, v, max)
-	}
-	return v, nil
-}
-
-func (le *LittleEndian) Uint64Within(min, max uint64) (uint64, error) {
-	b, err := le.ByteStream.TakeExactly(8)
-	if err != nil {
-		return 0, err
-	}
-	v := leOrder.Uint64(b)
-	if v < min || v > max {
-		return v, fmt.Errorf("bytestream: uint64 not in range %d <= %d <= %d", min, v, max)
-	}
-	return v, nil
-}
-
-func (le *LittleEndian) Int8Within(min, max int8) (int8, error) {
-	uv, err := le.ByteStream.ReadByte()
-	if err != nil {
-		return 0, err
-	}
-	v := int8(uv)
-	if v < min || v > max {
-		return v, fmt.Errorf("bytestream: int8 not in range %d <= %d <= %d", min, v, max)
-	}
-	return v, nil
-}
-
-func (le *LittleEndian) Int16Within(min, max int16) (int16, error) {
-	b, err := le.ByteStream.TakeExactly(2)
-	if err != nil {
-		return 0, err
-	}
-	v := int16(leOrder.Uint16(b))
-	if v < min || v > max {
-		return v, fmt.Errorf("bytestream: int16 not in range %d <= %d <= %d", min, v, max)
-	}
-	return v, nil
-}
-
-func (le *LittleEndian) Int32Within(min, max int32) (int32, error) {
-	b, err := le.ByteStream.TakeExactly(4)
-	if err != nil {
-		return 0, err
-	}
-	v := int32(leOrder.Uint32(b))
-	if v < min || v > max {
-		return v, fmt.Errorf("bytestream: int32 not in range %d <= %d <= %d", min, v, max)
-	}
-	return v, nil
-}
-
-func (le *LittleEndian) Int64Within(min, max int64) (int64, error) {
-	b, err := le.ByteStream.TakeExactly(8)
-	if err != nil {
-		return 0, err
-	}
-	v := int64(leOrder.Uint64(b))
-	if v < min || v > max {
-		return v, fmt.Errorf("bytestream: int64 not in range %d <= %d <= %d", min, v, max)
-	}
-	return v, nil
-}
-
-func (le *LittleEndian) Uint8() (uint8, error) {
-	return le.ByteStream.ReadByte()
-}
-
-func (le *LittleEndian) Uint16() (uint16, error) {
-	b, err := le.ByteStream.TakeExactly(2)
-	if err != nil {
-		return 0, err
-	}
-	return leOrder.Uint16(b), nil
-}
-
-func (le *LittleEndian) Uint32() (uint32, error) {
-	b, err := le.ByteStream.TakeExactly(4)
-	if err != nil {
-		return 0, err
-	}
-	return leOrder.Uint32(b), nil
-}
-
-func (le *LittleEndian) Uint64() (uint64, error) {
-	b, err := le.ByteStream.TakeExactly(8)
-	if err != nil {
-		return 0, err
-	}
-	return leOrder.Uint64(b), nil
-}
-
-func (le *LittleEndian) Int8() (int8, error) {
-	uv, err := le.ByteStream.ReadByte()
-	if err != nil {
-		return 0, err
-	}
-	return int8(uv), nil
-}
-
-func (le *LittleEndian) Int16() (int16, error) {
-	b, err := le.ByteStream.TakeExactly(2)
-	if err != nil {
-		return 0, err
-	}
-	return int16(leOrder.Uint16(b)), nil
-}
-
-func (le *LittleEndian) Int32() (int32, error) {
-	b, err := le.ByteStream.TakeExactly(4)
-	if err != nil {
-		return 0, err
-	}
-	return int32(leOrder.Uint32(b)), nil
-}
-
-func (le *LittleEndian) Int64() (int64, error) {
-	b, err := le.ByteStream.TakeExactly(8)
-	if err != nil {
-		return 0, err
-	}
-	return int64(leOrder.Uint64(b)), nil
-}
-
-var beOrder = binary.BigEndian
-
-type BigEndian struct {
-	ByteStream
-}
-
-var _ Binary = &BigEndian{}
-
-func NewBigEndian(stream ByteStream) *BigEndian {
-	return &BigEndian{stream}
-}
-
-func (be *BigEndian) Uint8Within(min, max uint8) (uint8, error) {
-	v, err := be.ByteStream.ReadByte()
-	if err != nil {
-		return 0, err
-	}
-	if v < min || v > max {
-		return v, fmt.Errorf("bytestream: uint8 not in range %d <= %d <= %d", min, v, max)
-	}
-	return v, nil
-}
-
-func (be *BigEndian) Uint16Within(min, max uint16) (uint16, error) {
-	b, err := be.ByteStream.TakeExactly(2)
+func (bin *Binary) Uint16BEWithin(min, max uint16) (uint16, error) {
+	b, err := bin.ByteStream.TakeExactly(2)
 	if err != nil {
 		return 0, err
 	}
@@ -226,8 +65,32 @@ func (be *BigEndian) Uint16Within(min, max uint16) (uint16, error) {
 	return v, nil
 }
 
-func (be *BigEndian) Uint32Within(min, max uint32) (uint32, error) {
-	b, err := be.ByteStream.TakeExactly(4)
+func (bin *Binary) Uint32Within(order binary.ByteOrder, min, max uint32) (uint32, error) {
+	b, err := bin.ByteStream.TakeExactly(4)
+	if err != nil {
+		return 0, err
+	}
+	v := order.Uint32(b)
+	if v < min || v > max {
+		return v, fmt.Errorf("bytestream: uint32 not in range %d <= %d <= %d", min, v, max)
+	}
+	return v, nil
+}
+
+func (bin *Binary) Uint32LEWithin(min, max uint32) (uint32, error) {
+	b, err := bin.ByteStream.TakeExactly(4)
+	if err != nil {
+		return 0, err
+	}
+	v := leOrder.Uint32(b)
+	if v < min || v > max {
+		return v, fmt.Errorf("bytestream: uint32 not in range %d <= %d <= %d", min, v, max)
+	}
+	return v, nil
+}
+
+func (bin *Binary) Uint32BEWithin(min, max uint32) (uint32, error) {
+	b, err := bin.ByteStream.TakeExactly(4)
 	if err != nil {
 		return 0, err
 	}
@@ -238,8 +101,32 @@ func (be *BigEndian) Uint32Within(min, max uint32) (uint32, error) {
 	return v, nil
 }
 
-func (be *BigEndian) Uint64Within(min, max uint64) (uint64, error) {
-	b, err := be.ByteStream.TakeExactly(8)
+func (bin *Binary) Uint64Within(order binary.ByteOrder, min, max uint64) (uint64, error) {
+	b, err := bin.ByteStream.TakeExactly(8)
+	if err != nil {
+		return 0, err
+	}
+	v := order.Uint64(b)
+	if v < min || v > max {
+		return v, fmt.Errorf("bytestream: uint64 not in range %d <= %d <= %d", min, v, max)
+	}
+	return v, nil
+}
+
+func (bin *Binary) Uint64LEWithin(min, max uint64) (uint64, error) {
+	b, err := bin.ByteStream.TakeExactly(8)
+	if err != nil {
+		return 0, err
+	}
+	v := leOrder.Uint64(b)
+	if v < min || v > max {
+		return v, fmt.Errorf("bytestream: uint64 not in range %d <= %d <= %d", min, v, max)
+	}
+	return v, nil
+}
+
+func (bin *Binary) Uint64BEWithin(min, max uint64) (uint64, error) {
+	b, err := bin.ByteStream.TakeExactly(8)
 	if err != nil {
 		return 0, err
 	}
@@ -250,8 +137,8 @@ func (be *BigEndian) Uint64Within(min, max uint64) (uint64, error) {
 	return v, nil
 }
 
-func (be *BigEndian) Int8Within(min, max int8) (int8, error) {
-	uv, err := be.ByteStream.ReadByte()
+func (bin *Binary) Int8Within(min, max int8) (int8, error) {
+	uv, err := bin.ByteStream.ReadByte()
 	if err != nil {
 		return 0, err
 	}
@@ -262,8 +149,32 @@ func (be *BigEndian) Int8Within(min, max int8) (int8, error) {
 	return v, nil
 }
 
-func (be *BigEndian) Int16Within(min, max int16) (int16, error) {
-	b, err := be.ByteStream.TakeExactly(2)
+func (bin *Binary) Int16Within(order binary.ByteOrder, min, max int16) (int16, error) {
+	b, err := bin.ByteStream.TakeExactly(2)
+	if err != nil {
+		return 0, err
+	}
+	v := int16(order.Uint16(b))
+	if v < min || v > max {
+		return v, fmt.Errorf("bytestream: int16 not in range %d <= %d <= %d", min, v, max)
+	}
+	return v, nil
+}
+
+func (bin *Binary) Int16LEWithin(min, max int16) (int16, error) {
+	b, err := bin.ByteStream.TakeExactly(2)
+	if err != nil {
+		return 0, err
+	}
+	v := int16(leOrder.Uint16(b))
+	if v < min || v > max {
+		return v, fmt.Errorf("bytestream: int16 not in range %d <= %d <= %d", min, v, max)
+	}
+	return v, nil
+}
+
+func (bin *Binary) Int16BEWithin(min, max int16) (int16, error) {
+	b, err := bin.ByteStream.TakeExactly(2)
 	if err != nil {
 		return 0, err
 	}
@@ -274,8 +185,32 @@ func (be *BigEndian) Int16Within(min, max int16) (int16, error) {
 	return v, nil
 }
 
-func (be *BigEndian) Int32Within(min, max int32) (int32, error) {
-	b, err := be.ByteStream.TakeExactly(4)
+func (bin *Binary) Int32Within(order binary.ByteOrder, min, max int32) (int32, error) {
+	b, err := bin.ByteStream.TakeExactly(4)
+	if err != nil {
+		return 0, err
+	}
+	v := int32(order.Uint32(b))
+	if v < min || v > max {
+		return v, fmt.Errorf("bytestream: int32 not in range %d <= %d <= %d", min, v, max)
+	}
+	return v, nil
+}
+
+func (bin *Binary) Int32LEWithin(min, max int32) (int32, error) {
+	b, err := bin.ByteStream.TakeExactly(4)
+	if err != nil {
+		return 0, err
+	}
+	v := int32(leOrder.Uint32(b))
+	if v < min || v > max {
+		return v, fmt.Errorf("bytestream: int32 not in range %d <= %d <= %d", min, v, max)
+	}
+	return v, nil
+}
+
+func (bin *Binary) Int32BEWithin(min, max int32) (int32, error) {
+	b, err := bin.ByteStream.TakeExactly(4)
 	if err != nil {
 		return 0, err
 	}
@@ -286,8 +221,32 @@ func (be *BigEndian) Int32Within(min, max int32) (int32, error) {
 	return v, nil
 }
 
-func (be *BigEndian) Int64Within(min, max int64) (int64, error) {
-	b, err := be.ByteStream.TakeExactly(8)
+func (bin *Binary) Int64Within(order binary.ByteOrder, min, max int64) (int64, error) {
+	b, err := bin.ByteStream.TakeExactly(8)
+	if err != nil {
+		return 0, err
+	}
+	v := int64(order.Uint64(b))
+	if v < min || v > max {
+		return v, fmt.Errorf("bytestream: int64 not in range %d <= %d <= %d", min, v, max)
+	}
+	return v, nil
+}
+
+func (bin *Binary) Int64LEWithin(min, max int64) (int64, error) {
+	b, err := bin.ByteStream.TakeExactly(8)
+	if err != nil {
+		return 0, err
+	}
+	v := int64(leOrder.Uint64(b))
+	if v < min || v > max {
+		return v, fmt.Errorf("bytestream: int64 not in range %d <= %d <= %d", min, v, max)
+	}
+	return v, nil
+}
+
+func (bin *Binary) Int64BEWithin(min, max int64) (int64, error) {
+	b, err := bin.ByteStream.TakeExactly(8)
 	if err != nil {
 		return 0, err
 	}
@@ -298,60 +257,156 @@ func (be *BigEndian) Int64Within(min, max int64) (int64, error) {
 	return v, nil
 }
 
-func (be *BigEndian) Uint8() (uint8, error) {
-	return be.ByteStream.ReadByte()
+func (bin *Binary) Uint8() (uint8, error) {
+	return bin.ByteStream.ReadByte()
 }
 
-func (be *BigEndian) Uint16() (uint16, error) {
-	b, err := be.ByteStream.TakeExactly(2)
+func (bin *Binary) Uint16(order binary.ByteOrder) (uint16, error) {
+	b, err := bin.ByteStream.TakeExactly(2)
+	if err != nil {
+		return 0, err
+	}
+	return order.Uint16(b), nil
+}
+
+func (bin *Binary) Uint16LE() (uint16, error) {
+	b, err := bin.ByteStream.TakeExactly(2)
+	if err != nil {
+		return 0, err
+	}
+	return leOrder.Uint16(b), nil
+}
+
+func (bin *Binary) Uint16BE() (uint16, error) {
+	b, err := bin.ByteStream.TakeExactly(2)
 	if err != nil {
 		return 0, err
 	}
 	return beOrder.Uint16(b), nil
 }
 
-func (be *BigEndian) Uint32() (uint32, error) {
-	b, err := be.ByteStream.TakeExactly(4)
+func (bin *Binary) Uint32(order binary.ByteOrder) (uint32, error) {
+	b, err := bin.ByteStream.TakeExactly(4)
+	if err != nil {
+		return 0, err
+	}
+	return order.Uint32(b), nil
+}
+
+func (bin *Binary) Uint32LE() (uint32, error) {
+	b, err := bin.ByteStream.TakeExactly(4)
+	if err != nil {
+		return 0, err
+	}
+	return leOrder.Uint32(b), nil
+}
+
+func (bin *Binary) Uint32BE() (uint32, error) {
+	b, err := bin.ByteStream.TakeExactly(4)
 	if err != nil {
 		return 0, err
 	}
 	return beOrder.Uint32(b), nil
 }
 
-func (be *BigEndian) Uint64() (uint64, error) {
-	b, err := be.ByteStream.TakeExactly(8)
+func (bin *Binary) Uint64(order binary.ByteOrder) (uint64, error) {
+	b, err := bin.ByteStream.TakeExactly(8)
+	if err != nil {
+		return 0, err
+	}
+	return order.Uint64(b), nil
+}
+
+func (bin *Binary) Uint64LE() (uint64, error) {
+	b, err := bin.ByteStream.TakeExactly(8)
+	if err != nil {
+		return 0, err
+	}
+	return leOrder.Uint64(b), nil
+}
+
+func (bin *Binary) Uint64BE() (uint64, error) {
+	b, err := bin.ByteStream.TakeExactly(8)
 	if err != nil {
 		return 0, err
 	}
 	return beOrder.Uint64(b), nil
 }
 
-func (be *BigEndian) Int8() (int8, error) {
-	uv, err := be.ByteStream.ReadByte()
+func (bin *Binary) Int8() (int8, error) {
+	uv, err := bin.ByteStream.ReadByte()
 	if err != nil {
 		return 0, err
 	}
 	return int8(uv), nil
 }
 
-func (be *BigEndian) Int16() (int16, error) {
-	b, err := be.ByteStream.TakeExactly(2)
+func (bin *Binary) Int16(order binary.ByteOrder) (int16, error) {
+	b, err := bin.ByteStream.TakeExactly(2)
+	if err != nil {
+		return 0, err
+	}
+	return int16(order.Uint16(b)), nil
+}
+
+func (bin *Binary) Int16LE() (int16, error) {
+	b, err := bin.ByteStream.TakeExactly(2)
+	if err != nil {
+		return 0, err
+	}
+	return int16(leOrder.Uint16(b)), nil
+}
+
+func (bin *Binary) Int16BE() (int16, error) {
+	b, err := bin.ByteStream.TakeExactly(2)
 	if err != nil {
 		return 0, err
 	}
 	return int16(beOrder.Uint16(b)), nil
 }
 
-func (be *BigEndian) Int32() (int32, error) {
-	b, err := be.ByteStream.TakeExactly(4)
+func (bin *Binary) Int32(order binary.ByteOrder) (int32, error) {
+	b, err := bin.ByteStream.TakeExactly(4)
+	if err != nil {
+		return 0, err
+	}
+	return int32(order.Uint32(b)), nil
+}
+
+func (bin *Binary) Int32LE() (int32, error) {
+	b, err := bin.ByteStream.TakeExactly(4)
+	if err != nil {
+		return 0, err
+	}
+	return int32(leOrder.Uint32(b)), nil
+}
+
+func (bin *Binary) Int32BE() (int32, error) {
+	b, err := bin.ByteStream.TakeExactly(4)
 	if err != nil {
 		return 0, err
 	}
 	return int32(beOrder.Uint32(b)), nil
 }
 
-func (be *BigEndian) Int64() (int64, error) {
-	b, err := be.ByteStream.TakeExactly(8)
+func (bin *Binary) Int64(order binary.ByteOrder) (int64, error) {
+	b, err := bin.ByteStream.TakeExactly(8)
+	if err != nil {
+		return 0, err
+	}
+	return int64(order.Uint64(b)), nil
+}
+
+func (bin *Binary) Int64LE() (int64, error) {
+	b, err := bin.ByteStream.TakeExactly(8)
+	if err != nil {
+		return 0, err
+	}
+	return int64(leOrder.Uint64(b)), nil
+}
+
+func (bin *Binary) Int64BE() (int64, error) {
+	b, err := bin.ByteStream.TakeExactly(8)
 	if err != nil {
 		return 0, err
 	}
