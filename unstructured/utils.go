60c9cea461791b184c09546e2c90c235a40cb752
Blake Williams <code@shabbyrobe.org>
2022-04-10 19:55:45 +1000

    unstructured: minor gardening

---
+++ unstructured/utils.go
@@ -0,0 +1,22 @@
+package unstructured
+
+import "fmt"
+
+func pathWithIdx(p string, idx int) string {
+	return fmt.Sprintf("%s/%d", p, idx)
+}
+
+func pathWithKey(p string, key string) string {
+	return fmt.Sprintf("%s/%s", p, key)
+}
+
+type value interface {
+	IsNull() bool
+	IsValid() bool
+	Kind() Kind
+	Path() string
+	Unwrap() any
+	Descend(part ...any) Value
+	TryDescend(part ...any) Value
+	Reject(err error)
+}
