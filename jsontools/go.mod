88813b1782cfdef36d75f4feb94b47e3e2797760
Blake Williams <code@shabbyrobe.org>
2022-10-26 00:37:42 +1100

    jsontools: Add polymorphic helpers

--- jsontools/go.mod
+++ jsontools/go.mod
@@ -1,3 +1,3 @@
 module github.com/shabbyrobe/golib/jsontools
 
-go 1.13
+go 1.19
---
+++ jsontools/poly.go
@@ -0,0 +1,112 @@
+package jsontools
+
+import (
+	"encoding/json"
+	"fmt"
+	"reflect"
+)
+
+func ToMap(v any) (out map[string]any, err error) {
+	bts, err := json.Marshal(v)
+	if err != nil {
+		return nil, err
+	}
+	if err := json.Unmarshal(bts, &out); err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func UnmarshalMap(m map[string]any, into any) (err error) {
+	bts, err := json.Marshal(m)
+	if err != nil {
+		return err
+	}
+	if err := json.Unmarshal(bts, into); err != nil {
+		return err
+	}
+	return nil
+}
+
+func MarshalPolymorphic[K ~string, V any](
+	v V,
+	kindKey string,
+	keyFn func(v V) K,
+) (raw []byte, err error) {
+	m, err := ToMap(v)
+	if err != nil {
+		return nil, err
+	}
+	m[kindKey] = string(keyFn(v))
+	return json.Marshal(m)
+}
+
+func MarshalIndentPolymorphic[K ~string](
+	v any,
+	kindKey string,
+	keyFn func(v any) K,
+	indent string,
+) (raw []byte, err error) {
+	m, err := ToMap(v)
+	if err != nil {
+		return nil, err
+	}
+	m[kindKey] = string(keyFn(v))
+	return json.MarshalIndent(m, "", indent)
+}
+
+func UnmarshalPolymorphic[K ~string, V any](
+	raw []byte,
+	kindKey string,
+	factory func(K) (V, error),
+	into *V,
+) (err error) {
+	var tmp map[string]any
+	if err := json.Unmarshal(raw, &tmp); err != nil {
+		return err
+	}
+
+	kindAny, ok := tmp[string(kindKey)]
+	if !ok {
+		return fmt.Errorf("key %q not found in object", kindKey)
+	}
+
+	kind, ok := kindAny.(string)
+	if !ok {
+		return fmt.Errorf("key %q is not a string, found %T", kindKey, kindAny)
+	}
+
+	v, err := factory(K(kind))
+	if err != nil {
+		return err
+	}
+
+	// XXX: We need to do some gymnastics here otherwise json.Unmarshal can't see the
+	// concrete type emitted by our factory (which will have an interface as its return
+	// type). We also need to take special care if we are using non-pointer types as the
+	// values inside the interface.
+	vv := reflect.ValueOf(v)
+	ptr := false
+	if vv.Kind() != reflect.Pointer {
+		vp := reflect.New(vv.Type())
+		vp.Elem().Set(vv)
+		vv = vp
+		ptr = true
+	}
+
+	delete(tmp, kindKey)
+	vi := vv.Interface()
+	if err := UnmarshalMap(tmp, vi); err != nil {
+		return err
+	}
+
+	// If we nested a pointer before unmarshalling, we need to unwrap it again:
+	if ptr {
+		vv := reflect.ValueOf(vi).Elem()
+		*into = vv.Interface().(V)
+	} else {
+		*into = vi.(V)
+	}
+
+	return nil
+}
