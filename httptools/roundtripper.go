deeb0adfe33ab34b8f4cf34db887f7bc913f1303
Blake Williams <code@shabbyrobe.org>
2024-07-21 12:22:45 +1000

    Remove roundtripper.go from httptools

--- httptools/roundtripper.go
+++
@@ -1,9 +0,0 @@
-package httptools
-
-import "net/http"
-
-type RoundTripperFunc func(*http.Request) (*http.Response, error)
-
-func (f RoundTripperFunc) RoundTrip(rq *http.Request) (rs *http.Response, err error) {
-	return f(rq)
-}
